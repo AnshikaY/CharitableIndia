1c8f511b9863841217f81024b07a6e212305b355
Anshika <yanshika00@gmail.com>
2021-04-03 13:46:55 +0530

    havoc

--- ngo_website/settings.py
+++ ngo_website/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ngo_website.urls'
+ROOT_URLCONF = 'ngo_website_1.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ngo_website.wsgi.application'
+WSGI_APPLICATION = 'ngo_website_1.wsgi.application'
 
 
 # Database
