de6f7a575c332c14eb507bbd20056b31e7e348d5
Anshika <yanshika00@gmail.com>
2021-04-04 00:06:51 +0530

    rm collectstatic

--- static/js/feedback.js
+++
@@ -1,7 +0,0 @@
-
-// FEEDBACK
-
-
-function feed_info(){
-  alert("Thank you for your valuable feedback!");
-}
--- static/js/home.js
+++
@@ -1,5 +0,0 @@
-
-// HOMEPAGE
-
-
-var start = alert("This website uses cookies to offer you a better browsing experience. By continuing, you agree to its use. To disable, you can change your browser settings at any time. However this might impair the working of our website.");
