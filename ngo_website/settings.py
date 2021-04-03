87c1c0489410f274e47639332db7bc8b7743033b
Anshika <yanshika00@gmail.com>
2021-04-03 22:33:55 +0530

    django-heroku

--- ngo_website/settings.py
+++ ngo_website/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+import django_heroku
+# import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +119,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+django_heroku.settings(locals())
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 
