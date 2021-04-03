cbc7c47e1eed62ca9da25ee9578573485f131ba7
Anshika <yanshika00@gmail.com>
2021-04-03 22:46:04 +0530

    static files

--- ngo_website/settings.py
+++ ngo_website/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 django_heroku.settings(locals())
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '/static/'),
