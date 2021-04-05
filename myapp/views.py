6ef2d972c9ca0c4aeabd5d998a04476174378bbf
AnshikaY <46274445+AnshikaY@users.noreply.github.com>
2021-04-05 15:10:31 +0530

    try-except

--- myapp/views.py
+++ myapp/views.py
@@ -10,59 +10,66 @@ def about(request):
 
 def donate(request):
     if request.method == "POST":
-        fname = request.POST.get('fname',False)
-        lname = request.POST.get('lname',False)
-        mail = request.POST.get('mail',False)
-        code = int(request.POST.get('code',False))
-        number = int(request.POST.get('number',False))
-        street_address = request.POST.get('street_address',False)
-        street_address_2 = request.POST.get('street_address_2',False)
-        city = request.POST.get('city',False)
-        state = request.POST.get('state',False)
-        pincode = int(request.POST.get('pincode',False))
-        amount = float(request.POST.get('amount',False))
-        mop = request.POST.get('mop',False)
-        account_no = int(request.POST.get('account_no',False))
-        same = request.POST.get('same',False)
-        confirm = request.POST.get('confirm',False)
-        if confirm == "on":
-            confirm = True
-        else:
-            confirm = False
-        pre = Donation(fname=str(fname),lname=str(lname),mail=str(mail),code=str(code),
-                       number=str(number),street_address=str(street_address),street_address_2=str(street_address_2),
-                       city=str(city),state=str(state),pincode=str(pincode),amount=str(amount),mop=str(mop),
-                       account_no=str(account_no),same=str(same),confirm=str(confirm)) #ORM
-        pre.save()
-        # print(request.POST)
+        try:
+            fname = request.POST.get('fname',False)
+            lname = request.POST.get('lname',False)
+            mail = request.POST.get('mail',False)
+            code = int(request.POST.get('code',False))
+            number = int(request.POST.get('number',False))
+            street_address = request.POST.get('street_address',False)
+            street_address_2 = request.POST.get('street_address_2',False)
+            city = request.POST.get('city',False)
+            state = request.POST.get('state',False)
+            pincode = int(request.POST.get('pincode',False))
+            amount = float(request.POST.get('amount',False))
+            mop = request.POST.get('mop',False)
+            account_no = int(request.POST.get('account_no',False))
+            same = request.POST.get('same',False)
+            confirm = request.POST.get('confirm',False)
+            if confirm == "on":
+                confirm = True
+            else:
+                confirm = False
+            pre = Donation(fname=str(fname),lname=str(lname),mail=str(mail),code=str(code),
+                           number=str(number),street_address=str(street_address),street_address_2=str(street_address_2),
+                           city=str(city),state=str(state),pincode=str(pincode),amount=str(amount),mop=str(mop),
+                           account_no=str(account_no),same=str(same),confirm=str(confirm)) #ORM
+            pre.save()
+            # print(request.POST)
+        except Exception as e:
+            print(e)
     return render(request, "donate.html", {})
     # return redirect('donation')
 
 def join(request):
     if request.method == "POST":
-        fname = request.POST.get('fname', False)
-        lname = request.POST.get('lname', False)
-        same = request.POST.get('same', False)
-        dob = request.POST.get('dob', False)
-        mail = request.POST.get('mail', False)
-        code = int(request.POST.get('code', False))
-        number = int(request.POST.get('number', False))
-        street_address = request.POST.get('street_address', False)
-        street_address_2 = request.POST.get('street_address_2', False)
-        city = request.POST.get('city', False)
-        state = request.POST.get('state', False)
-        pincode = int(request.POST.get('pincode', False))
-        mp = request.POST.get('mp', False)
-        text = request.POST.get('text', False)
-        confirm = request.POST.get('confirm', False)
-        if confirm == "on":
-            confirm = True
-        else:
-            confirm = False
-        pre1 = Membership(fname=str(fname),lname=str(lname),same=str(same),dob=str(dob),mail=str(mail),code=str(code),
-                          number=str(number),street_address=str(street_address),street_address_2=str(street_address_2),
-                          city=str(city),state=str(state),pincode=str(pincode),mp=str(mp),text=str(text),confirm=str(confirm))  # ORM
-        pre1.save()
+        try:
+            fname = request.POST.get('fname', False)
+            lname = request.POST.get('lname', False)
+            same = request.POST.get('same', False)
+            dob = request.POST.get('dob', False)
+            mail = request.POST.get('mail', False)
+            code = int(request.POST.get('code', False))
+            number = int(request.POST.get('number', False))
+            street_address = request.POST.get('street_address', False)
+            street_address_2 = request.POST.get('street_address_2', False)
+            city = request.POST.get('city', False)
+            state = request.POST.get('state', False)
+            pincode = int(request.POST.get('pincode', False))
+            mp = request.POST.get('mp', False)
+            text = request.POST.get('text', False)
+            confirm = request.POST.get('confirm', False)
+            if confirm == "on":
+                confirm = True
+            else:
+                confirm = False
+
+            pre1 = Membership(fname=str(fname),lname=str(lname),same=str(same),dob=str(dob),mail=str(mail),code=str(code),
+                              number=str(number),street_address=str(street_address),street_address_2=str(street_address_2),
+                              city=str(city),state=str(state),pincode=str(pincode),mp=str(mp),text=str(text),confirm=str(confirm))  # ORM
+            pre1.save()
+        except Exception as e:
+            print(e)
     return render(request, "join.html", {})
 
 def faq(request):
@@ -70,12 +77,15 @@ def faq(request):
 
 def feedback(request):
     if request.method == "POST":
-        fname = request.POST.get('fname', False)
-        lname = request.POST.get('lname', False)
-        mail = request.POST.get('mail', False)
-        text = request.POST.get('text', False)
-        pre2 = Feedback(fname=str(fname),lname=str(lname),mail=str(mail),text=str(text))
-        pre2.save()
+        try:
+            fname = request.POST.get('fname', False)
+            lname = request.POST.get('lname', False)
+            mail = request.POST.get('mail', False)
+            text = request.POST.get('text', False)
+            pre2 = Feedback(fname=str(fname),lname=str(lname),mail=str(mail),text=str(text))
+            pre2.save()
+        except Exception as e:
+            print(e)
     return render(request, "feedback.html", {})
 
 def contact(request):
